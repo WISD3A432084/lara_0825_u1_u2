7d6d665fd4d1087d6a6a34a647789e7686fe95ba
Xin <lovemarks19@gmail.com>
2017-11-01 11:26:35 +0800

    1101修改_讓 is_feature 欄位在 content_2 欄位後面

--- database/migrations/2017_10_24_153804_add_is_feature_in_posts_table.php
+++ database/migrations/2017_10_24_153804_add_is_feature_in_posts_table.php
@@ -15,7 +15,7 @@ class AddIsFeatureInPostsTable extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             $table->string('title', 500)->change();
-            $table->boolean('is_feature')->default(false)->after('content');
+            $table->boolean('is_feature')->default(false)->after('content_2');
         });
     }
 
