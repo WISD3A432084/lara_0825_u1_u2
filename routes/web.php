6b9a91a75b33f594ba807b1e511d0523c9d9a430
Xin <lovemarks19@gmail.com>
2017-11-05 13:54:51 +0800

    練習 6：了解 Model 和 Collection 的差異_取得 Collection

--- routes/web.php
+++ routes/web.php
@@ -49,5 +49,10 @@ Route::get('/test', function () {
 
     /*\App\Post::destroy(2);*/
 
-    \App\Post::destroy(3, 5, 7);
+    /*\App\Post::destroy(3, 5, 7);*/
+
+    $allPosts = \App\Post::all();
+    dd($allPosts);
+    $featuredPosts = \App\Post::where('is_feature', 1)->get();
+    dd($featuredPosts);
 });
