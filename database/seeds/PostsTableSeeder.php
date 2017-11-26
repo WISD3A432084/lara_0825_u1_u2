64665a5a5c4b9fd77280ca5cab082d85981873c3
Xin <lovemarks19@gmail.com>
2017-11-26 23:21:13 +0800

    讓資料有時間序列性、隨機感_rand 函式產⽣生隨機數字、使⽤ Carbon 控制日期時間產生間距

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Post;
+use Carbon\Carbon;
 
 class PostsTableSeeder extends Seeder
 {
@@ -14,10 +15,14 @@ class PostsTableSeeder extends Seeder
     {
         //
         Post::truncate();
+        $total = 20;
         foreach (range(1,20) as $number){
             Post::create([
                 'title' => 'title'.$number,
                 'content_2' => 'content_2'.$number,
+                'is_feature' => rand(0,1),
+                'created_at' => Carbon::now()->subDays($total - $number),
+                'updated_at' => Carbon::now()->subDays($total - $number),
             ]);
         }
     }
